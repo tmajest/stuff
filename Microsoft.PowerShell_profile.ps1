a3ffec841468211ae3d5720799d378e8c34c87d0
tmajest <tmajest@microsoft.com>
2014-12-15 10:52:06 -0800

    Added powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+﻿
+
+
+# Load posh-git example profile
+. 'C:\GitHub\posh-git\profile.example.ps1'
+
+$GitPromptSettings.EnableFileStatus = $false
+
+Import-Module PSReadLine 
+Set-PSReadlineKeyHandler -Key Ctrl+W -Function BackwardKillWord
+Set-PSReadlineKeyHandler -Key Ctrl+l -Function ClearScreen
+Set-PSReadlineKeyHandler -Key Ctrl+k -Function ForwardDeleteLine
+Set-PSReadlineKeyHandler -Key Ctrl+a -Function BeginningOfLine
+Set-PSReadlineKeyHandler -Key Ctrl+e -Function EndOfLine
