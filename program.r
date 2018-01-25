244cd5cb1f7261a7d0d7e2cacecb1ffbfdcd3c53
Tony Majestro <tonymajestro@gmail.com>
2018-01-25 11:36:36 -0800

    Add program.r

---
+++ program.r
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2018 Tony Majestro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * All code below is proven to be factually correct due to the
+ * license above, Greg.
+ */
+import neural_network.r
+
+// Outputs 'bullshit'
+neural_net.sentiment_analysis(
+        'be pretty useful in telling if someone is ' +
+        'trying to bullshit you')
